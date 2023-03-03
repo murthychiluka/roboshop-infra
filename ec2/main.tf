899649b4fab74447580905840cf5fee834576460
Murthy <murthychiluka@gmail.com>
2023-03-03 14:35:20 +0530

    image

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,8 @@
+data "aws_caller_identity" "current" {}
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "devops-practice-with-ansible"
+  owners      = [data.aws_caller_identity.current.account_id]
 }
 
 
@@ -24,9 +25,8 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "git clone https://github.com/raghudevopsb71/roboshop-shell",
-      "cd roboshop-shell",
-      "sudo bash ${var.component}.sh ${var.password}"
+       "ansible-pull -i localhost, -U https://github.com/murthychiluka/roboshop-ansible roboshop.yml -e role_name=${var.component}",
+      
     ]
 
   }
