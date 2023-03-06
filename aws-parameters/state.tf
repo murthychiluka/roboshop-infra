26cd3a88bf9ddd5c529a32fc77c471f7378403db
Murthy <murthychiluka@gmail.com>
2023-03-06 21:49:14 +0530

    c

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {}
 }
-resource "aws_ssm_parameter" "foo" {
+resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name  = var.parameters[counr.index.name]
   type  = var.parameters[counr.index.type]
