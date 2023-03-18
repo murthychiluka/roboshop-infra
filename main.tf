f0ff7c9f999b93f2a61cc272b421bb93b46f67b2
Murthy <murthychiluka@gmail.com>
2023-03-18 20:38:01 +0530

    a

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "vpc" {
   source = "git::https://github.com/murthychiluka/tf-module-vpc.git"
   env = var.env
  
- for_each = var.vpc 
- cidr = each.value["vpc_cidr"]
+  for_each = var.vpc 
+  cidr = each.value["vpc_cidr"]
 }
 
  
