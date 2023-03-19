c7d61837e2fbf88259b5e8545c3222aab36cdc79
Murthy <murthychiluka@gmail.com>
2023-03-19 14:31:06 +0530

    s

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 module "vpc" {
   source = "git::https://github.com/murthychiluka/tf-module-vpc.git"
-     env = var.env
-     tags = var.tags
- 
+  env    = var.env
+  tags   = var.tags
+
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
   public_subnets  = each.value["public_subnets"]
   private_subnets = each.value["private_subnets"]
-  
+
 }
 
- 
+output "vpc" {
+  value = module.vpc
+
+}
