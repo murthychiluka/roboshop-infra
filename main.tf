74ddbecde510e2d56ac770eb49b1f7480be29482
Murthy <murthychiluka@gmail.com>
2023-03-19 11:41:07 +0530

    a

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ module "vpc" {
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
   public_subnets  = each.value["public_subnets"]
+  private_subnets = each.value["private_subnets"]
+  
 }
 
  
