65660c4366b88aeedbf0857e06e982ed23300997
Murthy <murthychiluka@gmail.com>
2023-03-28 23:05:54 +0530

    d

--- main.tf
+++ main.tf
@@ -11,82 +11,82 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 
 }
-module "docdb" {
-  source = "git::https://github.com/murthychiluka/tf-module-docdb.git"
-  env    = var.env
-  tags   = var.tags
+# module "docdb" {
+#   source = "git::https://github.com/murthychiluka/tf-module-docdb.git"
+#   env    = var.env
+#   tags   = var.tags
 
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
 
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
+#   for_each                = var.docdb
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   skip_final_snapshot     = each.value["skip_final_snapshot"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
 
-}
+# }
 
-module "rds" {
-  source = "git::https://github.com/murthychiluka/tf-module-rds.git"
-  env    = var.env
-  tags   = var.tags
+# module "rds" {
+#   source = "git::https://github.com/murthychiluka/tf-module-rds.git"
+#   env    = var.env
+#   tags   = var.tags
 
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
 
-  for_each                = var.rds
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
-}
+#   for_each                = var.rds
+#   engine                  = each.value["engine"]
+#   engine_version          = each.value["engine_version"]
+#   backup_retention_period = each.value["backup_retention_period"]
+#   preferred_backup_window = each.value["preferred_backup_window"]
+#   no_of_instances         = each.value["no_of_instances"]
+#   instance_class          = each.value["instance_class"]
+# }
 
-module "elasticache" {
-  source = "git::https://github.com/murthychiluka/tf-module-elasticache.git"
-  env    = var.env
-  tags   = var.tags
+# module "elasticache" {
+#   source = "git::https://github.com/murthychiluka/tf-module-elasticache.git"
+#   env    = var.env
+#   tags   = var.tags
 
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
 
-  for_each        = var.elasticache
-  engine          = each.value["engine"]
-  engine_version  = each.value["engine_version"]
-  num_cache_nodes = each.value["num_cache_nodes"]
-  node_type       = each.value["node_type"]
+#   for_each        = var.elasticache
+#   engine          = each.value["engine"]
+#   engine_version  = each.value["engine_version"]
+#   num_cache_nodes = each.value["num_cache_nodes"]
+#   node_type       = each.value["node_type"]
 
-}
+# }
 
-module "rabbitmq" {
-  source = "git::https://github.com/murthychiluka/tf-module-rabbitmq.git"
-  env    = var.env
-  tags   = var.tags
+# module "rabbitmq" {
+#   source = "git::https://github.com/murthychiluka/tf-module-rabbitmq.git"
+#   env    = var.env
+#   tags   = var.tags
 
-  subnet_ids = local.db_subnet_ids
+#   subnet_ids = local.db_subnet_ids
 
-  for_each      = var.rabbitmq
-  instance_type = each.value["instance_type"]
+#   for_each      = var.rabbitmq
+#   instance_type = each.value["instance_type"]
 
-}
+# }
 
-module "alb" {
-  source = "git::https://github.com/murthychiluka/tf-module-alb.git"
-  env    = var.env
-  tags   = var.tags
+# module "alb" {
+#   source = "git::https://github.com/murthychiluka/tf-module-alb.git"
+#   env    = var.env
+#   tags   = var.tags
 
-  vpc_id = module.vpc["main"].vpc_id
+#   vpc_id = module.vpc["main"].vpc_id
 
-  for_each           = var.alb
-  name               = each.value["name"]
-  internal           = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
-  subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)
-  allow_cidr         = each.value["allow_cidr"]
+#   for_each           = var.alb
+#   name               = each.value["name"]
+#   internal           = each.value["internal"]
+#   load_balancer_type = each.value["load_balancer_type"]
+#   subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)
+#   allow_cidr         = each.value["allow_cidr"]
 
-}
+# }
 
 module "app" {
   source       = "git::https://github.com/murthychiluka/tf-module-app.git"
