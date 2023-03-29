9d04cb9b34e4984499ed658f83795e9b1a8bb932
Murthy <murthychiluka@gmail.com>
2023-03-29 21:34:25 +0530

    d

--- main.tf
+++ main.tf
@@ -62,17 +62,17 @@ module "elasticache" {
 
 }
 
-# module "rabbitmq" {
-#   source = "git::https://github.com/murthychiluka/tf-module-rabbitmq.git"
-#   env    = var.env
-#   tags   = var.tags
+module "rabbitmq" {
+  source = "git::https://github.com/murthychiluka/tf-module-rabbitmq.git"
+  env    = var.env
+  tags   = var.tags
 
-#   subnet_ids = local.db_subnet_ids
+  subnet_ids = local.db_subnet_ids
 
-#   for_each      = var.rabbitmq
-#   instance_type = each.value["instance_type"]
+  for_each      = var.rabbitmq
+  instance_type = each.value["instance_type"]
 
-# }
+}
 
 module "alb" {
   source = "git::https://github.com/murthychiluka/tf-module-alb.git"
