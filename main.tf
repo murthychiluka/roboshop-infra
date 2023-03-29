99ac6daad9e29be61cb57ecd607e84ca1f1ce4fa
Murthy <murthychiluka@gmail.com>
2023-03-29 18:24:42 +0530

    d

--- main.tf
+++ main.tf
@@ -31,36 +31,36 @@ module "docdb" {
 
 }
 
-# module "rds" {
-#   source = "git::https://github.com/murthychiluka/tf-module-rds.git"
-#   env    = var.env
-#   tags   = var.tags
+module "rds" {
+  source = "git::https://github.com/murthychiluka/tf-module-rds.git"
+  env    = var.env
+  tags   = var.tags
 
-#   subnet_ids = local.db_subnet_ids
+  subnet_ids = local.db_subnet_ids
 
-#   for_each                = var.rds
-#   engine                  = each.value["engine"]
-#   engine_version          = each.value["engine_version"]
-#   backup_retention_period = each.value["backup_retention_period"]
-#   preferred_backup_window = each.value["preferred_backup_window"]
-#   no_of_instances         = each.value["no_of_instances"]
-#   instance_class          = each.value["instance_class"]
-# }
+  for_each                = var.rds
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
+}
 
-# module "elasticache" {
-#   source = "git::https://github.com/murthychiluka/tf-module-elasticache.git"
-#   env    = var.env
-#   tags   = var.tags
+module "elasticache" {
+  source = "git::https://github.com/murthychiluka/tf-module-elasticache.git"
+  env    = var.env
+  tags   = var.tags
 
-#   subnet_ids = local.db_subnet_ids
+  subnet_ids = local.db_subnet_ids
 
-#   for_each        = var.elasticache
-#   engine          = each.value["engine"]
-#   engine_version  = each.value["engine_version"]
-#   num_cache_nodes = each.value["num_cache_nodes"]
-#   node_type       = each.value["node_type"]
+  for_each        = var.elasticache
+  engine          = each.value["engine"]
+  engine_version  = each.value["engine_version"]
+  num_cache_nodes = each.value["num_cache_nodes"]
+  node_type       = each.value["node_type"]
 
-# }
+}
 
 # module "rabbitmq" {
 #   source = "git::https://github.com/murthychiluka/tf-module-rabbitmq.git"
