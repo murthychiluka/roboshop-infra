4f1b8fa460495d74a7c73102545263a43826a996
murthychiluka <murthychiluka@gmail.com>
2023-04-24 18:45:35 +0530

    d

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 
 }
+/*
 module "docdb" {
   source = "git::https://github.com/murthychiluka/tf-module-docdb.git"
   env    = var.env
@@ -177,4 +178,34 @@ resource "null_resource" "load-gen" {
       "docker pull robotshop/rs-load"
     ]
   }
+}
+*/
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region        = "us-east-1"
+  cluster_name      = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key    = "~/.ssh/id_rsa.pub"
+  aws_subnet_id     = lookup(local.subnet_ids, "public", null)[0]
+  //ami_image_id        = data.aws_ami.ami.id
+  hosted_zone         = "murthychiluka.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+output "KUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
 }
